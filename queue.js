e164496e4b7b32a5e913dd5cd3a5709763bf7605
mmanso <mayara.manso@mercadolivre.com>
2021-02-16 18:04:07 -0300

    done

--- queue.js
+++ queue.js
@@ -1,5 +1,22 @@
 function createQueue() {
+  return {
+    elements: [],
+    size() {
+      return this.elements.length;
+    },
+    add(element) {
+      return this.elements.push(element);
+    },
+    peek() {
+      //return this.elements[0]
+      return [...this.elements].shift()
+    },
+    dequeue() {
+      if (this.elements.length === 0) throw new Error;
 
+      return this.elements.shift()
+    }
+  }
 }
 
 module.exports = { createQueue }
